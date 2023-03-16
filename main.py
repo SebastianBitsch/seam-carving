00164608a318f992b5adb4eea87743d47bd30ca3
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2023-03-16 14:43:06 +0100

    cleaned up imports and added to gitignore and req

--- main.py
+++ main.py
@@ -1,98 +1,21 @@
-import os
-from random import shuffle
-import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
+import matplotlib.pyplot as plt
 
 from gradientmagniture import SobelGradientMagnitude, ScharrGradientMagnitude, ExactGradientMagnitude
 from seamcarve import SeamCarver
 
 
-def plot(im, size = (10,10), cbar=False, cmap="viridis", title="Image"):
-    height, width = im.shape[:2]
-    # height *= 2
-    # width *= 2
-    margin=200 # pixels
-    dpi=100. # dots per inch
-
-    figsize=((width+4*margin)/dpi, (height+2*margin)/dpi) # inches
-    left = margin/dpi/figsize[0] #axes ratio
-    bottom = margin/dpi/figsize[1]
-
-    fig = plt.figure(figsize=figsize, dpi=dpi)
-    plt.title(title, loc="left")
-
-    fig.subplots_adjust(left=left, bottom=bottom, right=1.-3*left, top=1.-bottom)
-        
-    plt.imshow(im, cmap=cmap)
-    if cbar:
-        cbar_ax = fig.add_axes([1 - 3*left, bottom, 0.01, 1.-2*bottom])
-        plt.colorbar(cax=cbar_ax)
-    plt.show()
-
-
-def plot_demo(im):
-
-    grad = ExactGradientMagnitude()
-    carver = SeamCarver(im, grad)
-
-    num_seams = 100
-    carver.carve_seams(vertically = True, num_seams=num_seams)
-
-    # Plot original image
-    plot(carver.im, title = "1: Original image")
-    plot(carver.im_gray, title = "2: Convert to gray scale", cmap="gray")
-    plot(carver.im_grad, title = "3: Compute the gradient magnitude per pixel", cbar=True, cmap="gray")
-    plot(carver.cumulative, title = "4: Sum minimum gradients from the bottom up", cbar=True, cmap="gray")
-    # Draw seams
-    plot(carver.draw_seams(carver.cumulative, 1), title=f"5: Find cheapest path from bottom to top using dynamic programming")
-    for i in range(5, len(carver.seams), 5):
-        plot(carver.draw_seams(carver.cumulative, i), title=f"5: Find ({i}) cheapest paths from bottom to top using dynamic programming")
-    plot(carver.draw_seams(carver.cumulative, len(carver.seams)), title=f"5: Find ({len(carver.seams)}) cheapest paths from bottom to top using dynamic programming")
-    seams_im = carver.draw_seams(carver.im, i)
-    plot(seams_im, title=f"6: All seams carved")
-
-    # Plot Removed slices ontop cummulative image
-    for i in [0.25, 0.2, 0.15, 0.1, 0.05]:
-        res1 = carver.remove_seams(carver.im, carve_threshold=i)
-        res2 = carver.remove_seams(seams_im, carve_threshold=i)
-        im_width = carver.im.shape[1]
-        im_width_new = res1.shape[1]
-        plot(res2, title = f"7: Shink image by removing columns with {int(i*100)}% seams ({im_width - im_width_new}px removed)")
-        plot(res1, title = f"7: Shink image by removing columns with {int(i*100)}% seams ({im_width - im_width_new}px removed)")
-    
-
 if __name__ == "__main__":
 
-    # path = "images"
-    # im_names = [f for f in os.listdir(path) if not f.startswith('.')]
-
-    # # im_names = ["dolphins.jpg"]
-    # shuffle(im_names)
-
-    # for im_name in im_names:
-    #     im = Image.open(f"{path}/{im_name}")
-
-    #     # 1. Plot for demo
-    #     plot_demo(im)
-
-
-    # # 2. Use as library
-    # grad = ExactGradientMagnitude()
-    # carver = SeamCarver(im, grad)
-
-    # res = carver.compress()
-    # plot(res)
-    im = Image.open("images/tower.png").convert("L")
+    im = Image.open("images/waterfall.jpg")
     grad = ExactGradientMagnitude()
 
     carver = SeamCarver(im, grad)
 
-    new_im = carver.seam_carve(n_seams=50)
+    small_im = carver.carve(n_seams = 50)
 
-    plt.imshow(new_im, interpolation="none")
+    plt.imshow(small_im, interpolation="none")
     plt.show()
 
-    print(new_im.shape)
 
     
--- seamcarve.py
+++ seamcarve.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 from tqdm import tqdm
 
@@ -10,13 +9,12 @@ class SeamCarver:
 
     def __init__(self, image: Image, grad_func: GradientMagnitude) -> None:
         self.im = np.asarray(image)
-
-        if len(self.im.shape) < 3:
-            self.im = self.im[..., np.newaxis] # get the image on the shape (x,y,1)
         self.grad_func = grad_func
         
+        if len(self.im.shape) < 3:
+            self.im = self.im[..., np.newaxis] # get the image on the shape (x,y,1)
     
-    def compute_energy_map(self, im):
+    def _compute_energy_map(self, im):
         """ Compute the energy using the gradient function """
         h,w,d = im.shape
 
@@ -25,7 +23,8 @@ class SeamCarver:
             energy_map += self.grad_func.grad(im[:,:,i])
         return energy_map
 
-    def compute_cost_map(self, energy_map):
+    def _compute_cost_map(self, energy_map):
+        """ Compute the cumulative cost moving top-down in the image """
         cost = np.copy(energy_map)
         h,w = cost.shape
 
@@ -35,7 +34,8 @@ class SeamCarver:
                 cost[i,j] += np.min(cost[i-1,j-offset:j+2])
         return cost
 
-    def compute_min_energy_seam(self, cost_map):
+    def _compute_min_energy_seam(self, cost_map):
+        """ Returns a mask of the same size as the cost-map containing the locations of the seam """
         seam_mask = np.ones_like(cost_map, dtype=bool)
         h,_ = cost_map.shape
 
@@ -50,19 +50,21 @@ class SeamCarver:
             seam_mask[h-i, last_index] = False
         return seam_mask
     
-    def remove_seam(self, im, seam_mask, n_seams=1):
+    def _remove_seam(self, im, seam_mask, n_seams=1):
+        """ Remove a seam based on a mask of the same size as the image """
         h,w,d = im.shape
         return im[seam_mask].reshape(h, w-n_seams,d)
 
-    def seam_carve(self, n_seams = 20):
-    
+
+    def carve(self, n_seams = 20):
+        """ Remove n seams from the image """
         new_im = np.copy(self.im)
         
         for _ in tqdm(range(n_seams)):
             
-            energy_map = self.compute_energy_map(new_im)
-            cost_map = self.compute_cost_map(energy_map)
-            min_energy_seam = self.compute_min_energy_seam(cost_map)
-            new_im = self.remove_seam(new_im, min_energy_seam)
+            energy_map = self._compute_energy_map(new_im)
+            cost_map = self._compute_cost_map(energy_map)
+            min_energy_seam = self._compute_min_energy_seam(cost_map)
+            new_im = self._remove_seam(new_im, min_energy_seam)
 
         return new_im
